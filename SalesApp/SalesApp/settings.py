a61c892ecff9278756b3932c8d4381e53cc9b823
Gael <gaelthegoodboy@gmail.com>
2023-02-11 14:23:52 +0800

    Working to be a future developer

--- SalesApp/SalesApp/settings.py
+++ SalesApp/SalesApp/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+
+'''STATICFILES_DIRS = (
+    os.path.join (BASE_DIR, 'static')
+)'''
 #STATICFILES_DIRS = [BASE_DIR / './Inventory/static']
 
 # Default primary key field type
