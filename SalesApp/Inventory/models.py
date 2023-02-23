c00fc7077ffe347d6ccea393b01f75bb6202818c
Gael Estrera <gaelestrera@gmail.com>
2023-02-23 14:20:37 +0800

    ERD Database followed Order follow up

--- SalesApp/Inventory/models.py
+++ SalesApp/Inventory/models.py
@@ -1,16 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    ShipperName = models.CharField(max_length=30)
+    OceanVessel = models.CharField(max_length=50)
+    LocalVessel =  models.CharField(max_length=50)
+    LocalVesselOrigin = models.CharField(max_length=50)
+    objects = models.Manager()
+
+class NotifyParty(models.Model):
+    Name = models.CharField(max_length=50)
+    Address = models.CharField(max_length=50)
+    objects = models.Manager()
+
+class Consignee(models.Model):
+    BL = models.CharField(max_length=20, unique=True)
+    Name = models.CharField(max_length=30,unique=True)
+    PortDischarge = models.CharField(max_length=50)
+
+
 class Product(models.Model): #Steels, not food 
     objects = models.Manager()
     Name = models.CharField(max_length=30, unique=True)
     Image = models.ImageField(null= True, blank=True, upload_to="images/") #not have image if we dont have one, uploaded to images folder automatically
     Manufacturer = models.CharField(max_length=30)
+    Marks = models.CharField(max_length=50, unique=True) #manufacturer, location of manufacturer, color mark
     Length = models.CharField(max_length=20)
-    Cost = models.CharField(max_length=10)
+    Cost = models.CharField(max_length=10) #declared value USD
     Stock = models.BigIntegerField()
-    Description = models.CharField(null=True, max_length=500)
-    Contact = models.CharField(null= True, max_length=500)
+    Description = models.CharField(max_length=500)
+    Contact = models.CharField(max_length=500)
+
+#Products Composite Models
+
+class Product_desc(models.Model):
+    Description = models.CharField(max_length=500)
+    Marks = models.ForeignKey(Product.Marks, on_delete=models.CASCADE)
+
+class Product_Measurement(models.Model):
+    Measurement = models.CharField(max_length=30)
+    Marks = models.ForeignKey(Product.Marks, on_delete=models.CASCADE)
+
+class Product_GrossWeight(models.Model):
+    GrossWeight = models.CharField(max_length=40)
+    Marks = models.ForeignKey(Product.Marks, on_delete=models.CASCADE)
+
+
 
 
 class Userperson(models.Model):
@@ -51,7 +85,12 @@ class FinalOrder(models.Model):
     ProductInfo = models.CharField(max_length=400)
     Finished = models.IntegerField(default=0) #0 is for not yet finished
     OrderDate = models.DateField(auto_now_add=True)#auto_now_add=True)
-    OrderID = models.CharField(max_length=13, null=True)
+    #New Fields
+    Verification = models.CharField(max_length=13, null=True) #Orderid -> verification
+    Shipper = models.ForeignKey(Company.ShipperName, on_delete=models.CASCADE)
+    BL = models.ForeignKey(Consignee.BL, on_delete=models.CASCADE)
+    NotifyName = models.ForeignKey(NotifyParty.Name, on_delete=models.CASCADE)
+    Marks = models.ForeignKey(Product.Marks, on_delete=models.CASCADE) 
     objects = models.Manager()
 
 
@@ -61,6 +100,12 @@ class OrderedProduct(models.Model):
     quantity = models.IntegerField(default=0)
     totalcost = models.IntegerField(default=0)
     Finalorder = models.ForeignKey(FinalOrder, on_delete=models.CASCADE, default=None)
+    #New Fields
+    BL = models.CharField(max_length=20, unique=True)
+    Verificaton = models.ForeignKey(FinalOrder.Verification, on_delete=models.CASCADE)
+    Marks = models.ForeignKey(FinalOrder.Verification, on_delete=models.CASCADE)
+    Voyage = models.IntegerField(default=0)
+
     objects = models.Manager()
     
 
