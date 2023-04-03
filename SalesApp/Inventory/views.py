b8683ea85f911a789ad330538130b0a04cdc2270
Gael <gaelthegoodboy@gmail.com>
2023-04-04 02:17:33 +0800

    Working on Windows 7 except for zoneinfo module

--- SalesApp/Inventory/views.py
+++ SalesApp/Inventory/views.py
@@ -11,7 +11,7 @@ from django.utils import timezone
 from decimal import Decimal
 import random
 import string
-import zoneinfo
+
 
 def home(request):
     condition = ''
@@ -647,6 +647,7 @@ def ConfirmOrder(request,pk):
                     print('GOING BACK')
                     request.session['Order'] = None
                     request.session['productname'] = None
+                    request.session['Remarks'] = None
                     return redirect('createorder')
 
                 q = request.POST.get('drop')
